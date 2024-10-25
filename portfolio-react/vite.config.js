bcb4dfcf97103467d55ae26201bfa9aed80937d0
Dmitrii Mitin <dmitrii@Dmitriis-MacBook-Pro.local>
2024-10-25 15:44:15 -0400

    Update base path in vite.config.js for GitHub Pages

--- portfolio-react/vite.config.js
+++ portfolio-react/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'],
-  base: "/DmitriisPortfolio/portfolio-react/",
+  base: "/DmitriisPortfolio/",
 })
